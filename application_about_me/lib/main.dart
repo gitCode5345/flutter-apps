7c3a7e4b7bcdcecb3749f157a617507f9f03b238
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-10-21 21:00:20 +0300

    added multi-provider for processing multiple repositories

--- application_about_me/lib/main.dart
+++ application_about_me/lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:application_about_me/routing/app_router.dart';
-import 'package:application_about_me/ui/core/view_model/description_view_model.dart';
+import 'package:application_about_me/ui/add/view_model/description_view_model.dart';
 import 'package:application_about_me/data/repositories/description_repository.dart';
+import 'package:application_about_me/data/services/github_service.dart';
+import 'package:application_about_me/data/repositories/github_repository.dart';
+import 'package:application_about_me/ui/github_info/view_model/github_view_model.dart';
+
 
 void main() {
+  final gitHubService = GitHubService();
   final descriptionRepository = DescriptionRepository();
+  final gitHubRepository = GitHubRepository(gitHubService);
 
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => DescriptionViewModel(descriptionRepository),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DescriptionViewModel(descriptionRepository),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => GitHubViewModel(gitHubRepository),
+        ),
+      ],
       child: MaterialApp.router(
         routerConfig: appRouter,
       ),
