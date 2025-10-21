14051660fbaebda7498be4bd33c39f68870e6aac
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-10-21 21:01:20 +0300

    added update of existing resume entries

--- application_about_me/lib/data/repositories/description_repository.dart
+++ application_about_me/lib/data/repositories/description_repository.dart
@@ -1,16 +1,18 @@
 import 'package:application_about_me/domain/models/description_model.dart';
 
 class DescriptionRepository {
-  final List<Description> _description = [
-    Description(
-      title: "Flutter розробник",
-      text: "Flutter, Dart, Firebase, Git, Provider, GoRouter.",
-    ),
-  ];
+  final List<Description> _description = [];
 
   List<Description> getAllDescriptions() => List.unmodifiable(_description);
 
   void addDescription(Description description) {
     _description.add(description);
+  }
+
+  void updateDescription(Description oldDescription, Description newDescription) {
+    final index = _description.indexOf(oldDescription);
+    if (index != -1) {
+      _description[index] = newDescription;
+    }
   }
 }
