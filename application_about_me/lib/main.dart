2f0d6927d2a996eafc12d269b623b13d7bd8ab7a
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-11-03 20:23:16 +0200

    added a service and view model for loading the application theme, moved the creation of the main application widget to a separate class

--- application_about_me/lib/main.dart
+++ application_about_me/lib/main.dart
@@ -6,12 +6,17 @@ import 'package:application_about_me/data/repositories/description_repository.da
 import 'package:application_about_me/data/services/github_service.dart';
 import 'package:application_about_me/data/repositories/github_repository.dart';
 import 'package:application_about_me/ui/github_info/view_model/github_view_model.dart';
+import 'package:application_about_me/data/services/theme_service.dart';
+import 'package:application_about_me/ui/settings/view_model/theme_view_model.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final gitHubService = GitHubService();
   final descriptionRepository = DescriptionRepository();
   final gitHubRepository = GitHubRepository(gitHubService);
+  final themeService = ThemeService();
 
   runApp(
     MultiProvider(
@@ -22,10 +27,34 @@ void main() {
         ChangeNotifierProvider(
           create: (_) => GitHubViewModel(gitHubRepository),
         ),
+        ChangeNotifierProvider(
+          create: (_) => ThemeViewModel(themeService),
+        ),
       ],
-      child: MaterialApp.router(
-        routerConfig: appRouter,
-      ),
+      child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeViewModel = context.watch<ThemeViewModel>();
+
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      themeMode: themeViewModel.themeMode, 
+    );
+  }
 }
