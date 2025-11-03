bf36b11a7fd671dccc5ebc28a11e6e42293fb53b
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-11-03 20:31:21 +0200

    updated the work of the view model of descriptions: now we will poison data asynchronously and locally from the device. Also, operations with descriptions have been updated

--- application_about_me/lib/ui/add/view_model/description_view_model.dart
+++ application_about_me/lib/ui/add/view_model/description_view_model.dart
@@ -5,17 +5,37 @@ import 'package:application_about_me/data/repositories/description_repository.da
 class DescriptionViewModel extends ChangeNotifier {
   final DescriptionRepository _repository;
 
-  DescriptionViewModel(this._repository);
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  DescriptionViewModel(this._repository) {
+    loadDescriptions();
+  }
+
+  Future<void> loadDescriptions() async {
+    _isLoading = true;
+    notifyListeners();
+
+    await _repository.loadDescriptions();
+
+    _isLoading = false;
+    notifyListeners();
+  }
 
   List<Description> get descriptions => _repository.getAllDescriptions();
 
-  void addDescription(Description description) {
-    _repository.addDescription(description);
+  Future<void> addDescription(String title, String text) async {
+    await _repository.addDescription(title, text);
     notifyListeners();
   }
   
-  void updateDescription(Description oldDescription, Description newDescription) {
-    _repository.updateDescription(oldDescription, newDescription);
+  Future<void> updateDescription(Description oldDescription, Description newDescription) async {
+    await _repository.updateDescription(oldDescription, newDescription);
+    notifyListeners();
+  }
+
+  Future<void> deleteDescription(int id) async {
+    await _repository.deleteDescription(id);
     notifyListeners();
   }
 }
