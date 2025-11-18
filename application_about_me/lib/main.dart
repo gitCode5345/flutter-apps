0793e23bd67688af78d3f7252be41da51b8b2df4
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-11-18 18:53:01 +0200

    imported dependencies for advertising and loading data from .env file. added advertising initialization

--- application_about_me/lib/main.dart
+++ application_about_me/lib/main.dart
@@ -8,10 +8,21 @@ import 'package:application_about_me/data/repositories/github_repository.dart';
 import 'package:application_about_me/ui/github_info/view_model/github_view_model.dart';
 import 'package:application_about_me/data/services/theme_service.dart';
 import 'package:application_about_me/ui/settings/view_model/theme_view_model.dart';
-
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb) {
+    if (Platform.isAndroid || Platform.isIOS) {
+      await MobileAds.instance.initialize();
+    }
+  }
+  
+  await dotenv.load(fileName: ".env");
 
   final gitHubService = GitHubService();
   final descriptionRepository = DescriptionRepository();
